16a3975de35d5f4b600caaff76ce6fe2868b845f
nayeemnishaat <rxnayeemimi@gmail.com>
2022-09-22 23:00:36 +0600

    created serializer for errors

--- auth/src/errors/database-connection-error.ts
+++ auth/src/errors/database-connection-error.ts
@@ -1,9 +1,18 @@
 export class DatabaseConnectionError extends Error {
   reason = "Error connecting to database"; // Note: Available as ob.reason
+  statusCode = 500;
 
   constructor() {
     super();
 
     Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+  }
+
+  serializeErrors() {
+    return [
+      {
+        message: this.reason
+      }
+    ];
   }
 }
--- auth/src/errors/request-validation-error.ts
+++ auth/src/errors/request-validation-error.ts
@@ -1,6 +1,7 @@
 import { ValidationError } from "express-validator";
 
 export class RequestValidationError extends Error {
+  statusCode = 400;
   // errors: ValidationError[];
   // constructor(errors: ValidationError[]) {
   //   super();
@@ -13,5 +14,11 @@ export class RequestValidationError extends Error {
 
     // Important: When extending a built-in class like Error in TypeScript you need to set the prototype explicitly.
     Object.setPrototypeOf(this, RequestValidationError.prototype);
+  }
+
+  serializeErrors() {
+    return this.errors.map((error) => {
+      return { message: error.msg, field: error.param };
+    });
   }
 }
