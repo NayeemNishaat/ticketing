db7e0a1bc0649ad501f3ee8f3d10c060c30be466
nayeemnishaat <rxnayeemimi@gmail.com>
2022-09-23 09:22:17 +0600

    created CustomError abstract class

--- auth/src/errors/request-validation-error.ts
+++ auth/src/errors/request-validation-error.ts
@@ -1,7 +1,8 @@
 import { ValidationError } from "express-validator";
+import { CustomError } from "./custom-error";
 
 // Remark: Ideally this should be in a different file
-// Part: Using Interface another option is to use an Abstract Class
+// Part: Using Interface another option is to use an Abstract Class. Not using interface because in js world there is no interface but there is class and classes creted through abstract class has their class defination preserved.
 // interface customError {
 //   statusCode: number;
 //   serializeErrors(): {
@@ -11,7 +12,7 @@ import { ValidationError } from "express-validator";
 // }
 
 // export class RequestValidationError extends Error implements customError {
-export class RequestValidationError extends Error {
+export class RequestValidationError extends CustomError {
   statusCode = 400;
   // errors: ValidationError[];
   // constructor(errors: ValidationError[]) {
@@ -21,7 +22,7 @@ export class RequestValidationError extends Error {
 
   // Note: Equivalent to the above syntax
   constructor(public errors: ValidationError[]) {
-    super();
+    super("Invalid request parameters");
 
     // Important: When extending a built-in class like Error in TypeScript you need to set the prototype explicitly.
     Object.setPrototypeOf(this, RequestValidationError.prototype);
