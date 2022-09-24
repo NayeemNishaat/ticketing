c6c807640568117b861d1a681bd46a0460f8594e
nayeemnishaat <rxnayeemimi@gmail.com>
2022-09-24 12:57:58 +0600

    implemented not found error

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,6 +5,7 @@ import { signinRouter } from "./routes/signin";
 import { signupRouter } from "./routes/signup";
 import { signoutRouter } from "./routes/signout";
 import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
 app.use(express.json());
@@ -13,6 +14,10 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signupRouter);
 app.use(signoutRouter);
+
+app.get("*", (_req, _res, _next) => {
+  throw new NotFoundError(); // Manually throwing an error and it will be captured by the global error handler middleware in our case it will be the below "errorHandler" middleware.
+});
 
 app.use(errorHandler);
 
