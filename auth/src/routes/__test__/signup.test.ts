0e8676d6dcc89af959f88090d177f22d8f07043e
nayeemnishaat <rxnayeemimi@gmail.com>
2022-10-02 15:05:53 +0600

    wrote tests for signup request validation

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -10,3 +10,35 @@ it("returns a 201 on successful signup", async () => {
     })
     .expect(201);
 });
+
+it("returns a 400 with an invalid email", async () => {
+  return request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@testcom",
+      password: "password"
+    })
+    .expect(400);
+});
+
+it("returns a 400 with an invalid password", async () => {
+  await request(app) // Note: await/return both works
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "pa"
+    })
+    .expect(400);
+});
+
+it("returns a 400 if no email/password provided", async () => {
+  await request(app).post("/api/users/signup").send().expect(400);
+  await request(app) // Note: await for using multiple requests in a single test
+    .post("/api/users/signup")
+    .send({ email: "abc@123.com" })
+    .expect(400);
+  return request(app) // Note: await for using multiple requests in a single test
+    .post("/api/users/signup")
+    .send({ password: "abc123" })
+    .expect(400);
+});
--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -4,6 +4,7 @@ import { app } from "../app";
 
 let mongo: any;
 beforeAll(async () => {
+  process.env.JWT_KEY = "asdf";
   // Important: For mongodb libssl 1.1 is required which is not available on ubuntu 22.04 hence we need to use the following workaround
   // wget http://debian.mirror.ac.za/debian/pool/main/o/openssl/libssl1.1_1.1.1o-1_amd64.deb
   // sudo dpkg -i libssl1.1_1.1.1o-1_amd64.deb
