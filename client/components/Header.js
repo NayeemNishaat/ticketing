15de1313585e661492ec970850b08003d54e834f
Nayeem Nishaat <rxnayeemimi@gmail.com>
2022-10-16 00:56:31 +0600

    added signout  component

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,16 +1,30 @@
 import Link from "next/link";
 
 function Header({ currentUser }) {
+  const links = [
+    !currentUser && { label: "Sign Up", href: "/auth/signup" },
+    !currentUser && { label: "Sign In", href: "/auth/signin" },
+    currentUser && { label: "Sign Out", href: "/auth/signout" }
+  ]
+    .filter((linkConfig) => linkConfig)
+    .map(({ label, href }) => {
+      return (
+        <li className="nav-item" key={href}>
+          <Link href={href}>
+            <a className="nav-link"> {label}</a>
+          </Link>
+        </li>
+      );
+    });
+
   return (
-    <nav className="navbar navbar-light bg-light">
+    <nav className="navbar navbar-light bg-light px-5">
       <Link href="/">
         <a className="navbar-brand">GitTix</a>
       </Link>
 
       <div className="d-flex justify-content-end">
-        <ul className="nav d-flex align-items-center">
-          {currentUser ? "Sign Out" : "Sign IN/Up"}
-        </ul>
+        <ul className="nav d-flex align-items-center">{links}</ul>
       </div>
     </nav>
   );
