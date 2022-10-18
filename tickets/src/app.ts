bfb73c3aa1830a7bb6a9fa58f026aa7e311fef23
Nayeem Nishaat <rxnayeemimi@gmail.com>
2022-10-18 16:22:41 +0600

    init ticket svc

---
+++ tickets/src/app.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import "express-async-errors"; // Note: This is a package that allows us to throw errors inside async functions and it will be automatically caught by the global error handler middleware. Only need to importonce before creating the "app"!
+import cookieSession from "cookie-session";
+import { errorHandler, NotFoundError } from "@labyrinth-inc/ticketing-sdk";
+
+const app = express();
+app.set("trust proxy", true); // Note: Traffic is being proxied to our application through ingress-nginx. Hence bt default express doen't trust proxied https connection. That's why we are saying express to trust proxy.
+app.use(express.json());
+app.use(
+  cookieSession({ signed: false, secure: process.env.NODE_ENV !== "test" })
+);
+
+app.all("*", async (_req, _res, next) => {
+  // next(new NotFoundError()); // Note: For throwing async errors
+  throw new NotFoundError(); // Note: Manually throwing an error and it will be captured by the global error handler middleware in our case it will be the below "errorHandler" middleware. Important: This will not work if we don't have the "express-async-errors" package installed because async errors will not be automatically captured by express and this request will hang.
+});
+
+app.use(errorHandler);
+
+export { app };
---
+++ tickets/src/index.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import { app } from "./app";
+
+const start = async () => {
+  if (!process.env.JWT_KEY) throw new Error("JWT_KEY is not defined."); // Important: This error will never reach to the client because it's not originated in between request-respponse cycle.
+
+  try {
+    await mongoose.connect("mongodb://auth-mongo-svc:27017/auth"); // Note: "auth-mongo-svc" is the service name/cluster ip and "27017" is the port for the cluster ip service and "auth" is the db name.
+    console.log("MongoDB Online");
+  } catch (error) {
+    console.error(error);
+  }
+
+  app.listen(3000, () => {
+    console.log("Listening on port 3000");
+  });
+};
+start();
