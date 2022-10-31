759d2f2a33454df3d7ee392799ffc187bb9c1c5a
Nayeem Nishaat <rxnayeemimi@gmail.com>
2022-10-31 18:05:04 +0600

    wired up nats

--- client/pages/index.js
+++ client/pages/index.js
@@ -14,7 +14,7 @@ LandingPage.getInitialProps = async (context) => {
   // Note: To check if user is logged in or not the request is required to made in this serverside component.
   // await axios.get("/api/users/currentuser"); // Important: This reques will run inside the nextjs client pod. And in that pod on 127.0.0.1:80/api/users/currentuser nothing is running. Hence error occured.
   // Important: For communication between pods in the same nameshapece the url will be -> http://auth-svc/api/users/currentuser
-  // Important: For communication between pods in different nameshapeces the url will be -> http://nameOfTheService.namespace.svc.cluster.local/api/users/currentuser
+  // Important: For communication between pods in different nameshapeces the url will be -> http://nameOfTheService.nameOfThenamespace.svc.cluster.local/api/users/currentuser
   // Note: kubectl get namespace -> Get all namespaces
   // Note: kubectl get services -n ingress-nginx -> Get all services of ingress-nginx namespace.
 
--- ticket/src/index.ts
+++ ticket/src/index.ts
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import { app } from "./app";
+import { natsWrapper } from "./nats-wrapper";
 
 const start = async () => {
   if (!process.env.JWT_KEY) throw new Error("JWT_KEY is not defined."); // Important: This error will never reach to the client because it's not originated in between request-respponse cycle.
@@ -7,6 +8,8 @@ const start = async () => {
   if (!process.env.MONGO_URI) throw new Error("MONGO_URI is not defined.");
 
   try {
+    await natsWrapper.connect("ticketing", "hjijdjf", "http://nats-svc:4222");
+
     await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB Online (ticket svc)");
   } catch (error) {
@@ -18,3 +21,5 @@ const start = async () => {
   });
 };
 start();
+
+// Warning: Important: We will not create a NATS client in this file and export it. If we do so we will end up creating a circular export-import loop/dependency. Which in turn cause re-running of these two files back and forth and will cause error!
