d1b2837f70ab3f65f21bc0c2b66e6730a3d568ce
Nayeem Nishaat <rxnayeemimi@gmail.com>
2022-11-01 14:44:22 +0600

    implemented nats singleton

--- ticket/src/routes/new.ts
+++ ticket/src/routes/new.ts
@@ -3,6 +3,7 @@ import { body } from "express-validator";
 import { requireAuth, validateRequest } from "@labyrinth-inc/ticketing-sdk";
 import { Ticket } from "../models/ticket";
 import { TicketCreatedPublisher } from "../events/publishers/ticket-created-publisher";
+import { natsWrapper } from "../nats-wrapper";
 
 const router = express.Router();
 
@@ -26,12 +27,13 @@ router.post(
     });
 
     await ticket.save();
-    // await new TicketCreatedPublisher(client).publish({
-    //   id: ticket.id,
-    //   title: ticket.title,
-    //   price: ticket.price,
-    //   userId: ticket.userId
-    // });
+    await new TicketCreatedPublisher(natsWrapper.client).publish({
+      // Remark: client is a getter. So we can't call it.
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId
+    });
 
     res.status(201).send(ticket);
   }
