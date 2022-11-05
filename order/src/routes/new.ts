93a5d027fbc568f8d69a6f72e28dbb904a485a3e
Nayeem Nishaat <rxnayeemimi@gmail.com>
2022-11-06 00:27:50 +0600

    implemented finding of reserved tickets

--- order/src/routes/new.ts
+++ order/src/routes/new.ts
@@ -1,7 +1,15 @@
 import express, { Request, Response } from "express";
 import mongoose from "mongoose";
-import { requireAuth, validateRequest } from "@labyrinth-inc/ticketing-sdk";
+import {
+  requireAuth,
+  validateRequest,
+  NotFoundError,
+  BadRequestError
+} from "@labyrinth-inc/ticketing-sdk";
+import { OrderStatus } from "@labyrinth-inc/ticketing-sdk";
 import { body } from "express-validator";
+import { Ticket } from "../models/ticket";
+import { Order } from "../models/order";
 
 const router = express.Router();
 
@@ -17,6 +25,32 @@ router.post(
   ],
   validateRequest,
   async (req: Request, res: Response) => {
+    const { ticketId } = req.body;
+
+    // Part: Find the ticket the user is trying to order in the database
+    const ticket = await Ticket.findById(ticketId);
+    if (!ticket) throw new NotFoundError();
+
+    // Part: Make sure that this ticket is not already reserved
+    const existingOrder = await Order.findOne({
+      ticket,
+      status: {
+        $in: [
+          OrderStatus.Created,
+          OrderStatus.AwaitingPayment,
+          OrderStatus.Complete
+        ]
+      }
+    });
+
+    if (existingOrder) throw new BadRequestError("Ticket is already reserved");
+
+    // Part: Calculate an expiration date for this order
+
+    // Part: Build the order and save it to the database
+
+    // Part: Publish an event saying that an order was created
+
     res.send({});
   }
 );
