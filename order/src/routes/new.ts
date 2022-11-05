ed5c42703990e862cd663bd85ebe7f370509b5cf
Nayeem Nishaat <rxnayeemimi@gmail.com>
2022-11-05 20:40:56 +0600

    working on new order route handler

--- order/src/routes/new.ts
+++ order/src/routes/new.ts
@@ -1,9 +1,24 @@
 import express, { Request, Response } from "express";
+import mongoose from "mongoose";
+import { requireAuth, validateRequest } from "@labyrinth-inc/ticketing-sdk";
+import { body } from "express-validator";
 
 const router = express.Router();
 
-router.post("/api/order", async (req: Request, res: Response) => {
-  res.send({});
-});
+router.post(
+  "/api/order",
+  requireAuth,
+  [
+    body("ticketId")
+      .not()
+      .isEmpty()
+      .custom((string: string) => mongoose.Types.ObjectId.isValid(string))
+      .withMessage("TicketId must be provided")
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    res.send({});
+  }
+);
 
 export { router as newOrderRouter };
