1a668b225de02a004b98be075d5bb34393d2f3b7
Nayeem Nishaat <rxnayeemimi@gmail.com>
2022-11-06 11:12:18 +0600

    test suit setup

--- order/src/routes/new.ts
+++ order/src/routes/new.ts
@@ -13,6 +13,8 @@ import { Order } from "../models/order";
 
 const router = express.Router();
 
+const EXPIRATION_WINDOW_SECONDS = 1 * 60;
+
 router.post(
   "/api/order",
   requireAuth,
@@ -36,12 +38,23 @@ router.post(
     if (isReserved) throw new BadRequestError("Ticket is already reserved");
 
     // Part: Calculate an expiration date for this order
+    const expiration = new Date();
+    expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS);
 
     // Part: Build the order and save it to the database
+    const order = Order.build({
+      userId: req.currentUser!.id,
+      status: OrderStatus.Created,
+      expiresAt: expiration,
+      ticket
+    });
+
+    await order.save();
 
     // Part: Publish an event saying that an order was created
+    // TODO:
 
-    res.send({});
+    res.status(201).send(order);
   }
 );
 
