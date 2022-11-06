803dd9a0982000a8c8b44e08422b2aeec8f8c172
Nayeem Nishaat <rxnayeemimi@gmail.com>
2022-11-06 21:31:33 +0600

    implemented show ticket route

--- order/src/routes/show.ts
+++ order/src/routes/show.ts
@@ -1,9 +1,24 @@
 import express, { Request, Response } from "express";
+import {
+  requireAuth,
+  NotFoundError,
+  NotAuthorizedError
+} from "@labyrinth-inc/ticketing-sdk";
+import { Order } from "../models/order";
 
 const router = express.Router();
 
-router.get("/api/order/:orderId", async (req: Request, res: Response) => {
-  res.send({});
-});
+router.get(
+  "/api/order/:orderId",
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const order = await Order.findById(req.params.orderId).populate("ticket");
+    if (!order) throw new NotFoundError();
+
+    if (order.userId !== req.currentUser!.id) throw new NotAuthorizedError();
+
+    res.send(order);
+  }
+);
 
 export { router as showOrderRouter };
