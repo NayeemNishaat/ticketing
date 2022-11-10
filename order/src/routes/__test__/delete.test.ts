857796236609545106a4d32669fb81ff89e6d04d
Nayeem Nishaat <rxnayeemimi@gmail.com>
2022-11-10 18:16:51 +0600

    tested event publishing

--- order/src/routes/__test__/delete.test.ts
+++ order/src/routes/__test__/delete.test.ts
@@ -2,6 +2,7 @@ import request from "supertest";
 import { app } from "../../app";
 import { Ticket } from "../../models/ticket";
 import { Order, OrderStatus } from "../../models/order";
+import { natsWrapper } from "../../nats-wrapper";
 
 it("marks an order as cancelled", async () => {
   // Part: Create a ticket with Ticket Model
@@ -33,4 +34,29 @@ it("marks an order as cancelled", async () => {
   expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled);
 });
 
-it.todo("emits an order cancelled event");
+it("emits an order cancelled event", async () => {
+  // Part: Create a ticket with Ticket Model
+  const ticket = Ticket.build({
+    title: "concert",
+    price: 20
+  });
+  await ticket.save();
+
+  const user = global.signin();
+
+  // Part: Make a request to create an order
+  const { body: order } = await request(app)
+    .post("/api/order")
+    .set("Cookie", user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  // Part: Make a request to cancel the order
+  await request(app)
+    .delete(`/api/order/${order.id}`)
+    .set("Cookie", user)
+    .send()
+    .expect(204);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
+});
--- order/src/routes/__test__/new.test.ts
+++ order/src/routes/__test__/new.test.ts
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import { app } from "../../app";
 import { Order, OrderStatus } from "../../models/order";
 import { Ticket } from "../../models/ticket";
+import { natsWrapper } from "../../nats-wrapper";
 
 it("returns an error if the ticket does not exist", async () => {
   const ticketId = new mongoose.Types.ObjectId();
@@ -48,8 +49,20 @@ it("reserves a ticket", async () => {
     .set("Cookie", global.signin())
     .send({ ticketId: ticket.id })
     .expect(201);
-
-  // Note: Check if the order is created
 });
 
-it.todo("emits an order created event");
+it("emits an order created event", async () => {
+  const ticket = Ticket.build({
+    title: "concert",
+    price: 20
+  });
+  await ticket.save();
+
+  await request(app)
+    .post("/api/order")
+    .set("Cookie", global.signin())
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
+});
