20a88461199c372f2eb39557605eb5f1defe5716
Nayeem Nishaat <rxnayeemimi@gmail.com>
2022-11-13 14:31:20 +0600

    working on ticked created listener

---
+++ order/src/events/listeners/ticket-created-listener.ts
@@ -0,0 +1,24 @@
+import { Message } from "node-nats-streaming";
+import {
+  Listener,
+  TicketCreatedEvent,
+  Subjects
+} from "@labyrinth-inc/ticketing-sdk";
+import { Ticket } from "../../models/ticket";
+import { queueGroupName } from "./queue-group-name";
+
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+  readonly subject = Subjects.TicketCreated;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: TicketCreatedEvent["data"], msg: Message) {
+    const { title, price } = data;
+    const ticket = Ticket.build({
+      title,
+      price
+    });
+    await ticket.save();
+
+    msg.ack();
+  }
+}
