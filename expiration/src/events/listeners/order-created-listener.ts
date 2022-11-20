fdf993dfeb550604eea44a489e952cbef548e656
Nayeem Nishaat <rxnayeemimi@gmail.com>
2022-11-20 14:20:16 +0600

    wired up ticket created listener in expiration svc

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -5,6 +5,7 @@ import {
 } from "@labyrinth-inc/ticketing-sdk";
 import { queueGroupName } from "./queue-group-name";
 import { Message } from "node-nats-streaming";
+import { expirationQueue } from "../../queues/expiration-queue";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   subject: Subjects.OrderCreated = Subjects.OrderCreated;
@@ -13,14 +14,10 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
     // const delay = new Date(data.expiresAt).getTime() - new Date().getTime();
     // console.log("Waiting this many milliseconds to process the job:", delay);
-    // await expirationQueue.add(
-    //   {
-    //     orderId: data.id
-    //   },
-    //   {
-    //     delay
-    //   }
-    // );
-    // msg.ack();
+    await expirationQueue.add({
+      orderId: data.id
+    });
+
+    msg.ack();
   }
 }
