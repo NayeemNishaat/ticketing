e5b6355441ee9ca24d33350bbe5b02fcb865b48f
Nayeem Nishaat <rxnayeemimi@gmail.com>
2022-11-21 15:55:41 +0600

    init payment svc

---
+++ payment/src/index.ts
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+import { app } from "./app";
+import { natsWrapper } from "./nats-wrapper";
+
+const start = async () => {
+  if (!process.env.JWT_KEY) throw new Error("JWT_KEY is not defined."); // Important: This error will never reach to the client because it's not originated in between request-respponse cycle.
+
+  if (!process.env.MONGO_URI) throw new Error("MONGO_URI is not defined.");
+  if (!process.env.NATS_CLIENT_ID)
+    throw new Error("NATS_CLIENT_ID is not defined.");
+  if (!process.env.NATS_URL) throw new Error("NATS_URL is not defined.");
+  if (!process.env.NATS_CLUSTER_ID)
+    throw new Error("NATS_CLUSTER_ID is not defined.");
+
+  try {
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+    );
+
+    natsWrapper.client.on("close", () => {
+      console.log("NATS connection closed!");
+      process.exit();
+    });
+
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB Online (ticket svc)");
+  } catch (error) {
+    console.error(error);
+  }
+
+  app.listen(3000, () => {
+    console.log("Listening on port 3000 (ticket svc)");
+  });
+
+  process.on("SIGINT", () => natsWrapper.client.close());
+  process.on("SIGTERM", () => natsWrapper.client.close());
+};
+start();
+
+// Warning: Important: We will not create a NATS client in this file and export it. If we do so we will end up creating a circular export-import loop/dependency. Which in turn cause re-running of these two files back and forth and will cause error!
---
+++ payment/src/test/setup.ts
@@ -0,0 +1,58 @@
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+
+declare global {
+  function signin(): string[];
+}
+
+jest.mock("../nats-wrapper");
+
+let mongo: any;
+beforeAll(async () => {
+  process.env.JWT_KEY = "asdf";
+  // Important: For mongodb libssl 1.1 is required which is not available on ubuntu 22.04 hence we need to use the following workaround
+  // wget http://debian.mirror.ac.za/debian/pool/main/o/openssl/libssl1.1_1.1.1o-1_amd64.deb
+  // sudo dpkg -i libssl1.1_1.1.1o-1_amd64.deb
+  mongo = await MongoMemoryServer.create();
+  const mongoUri = mongo.getUri();
+
+  await mongoose.connect(mongoUri);
+});
+
+beforeEach(async () => {
+  jest.clearAllMocks();
+  const collections = await mongoose.connection.db.collections();
+
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  await mongo.stop();
+  await mongoose.connection.close();
+});
+
+global.signin = () => {
+  // Part: Build a JWT payload. { id, email }
+  const payload = {
+    id: new mongoose.Types.ObjectId().toHexString(),
+    email: "abc@xyz.ijk"
+  };
+
+  // Part: Create the JWT
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+
+  // Part: Build session Object. { jwt: JWT_TOKEN }
+  const session = { jwt: token };
+
+  // Part: Turn the session into JSON
+  const sessionJSON = JSON.stringify(session);
+
+  // Part: Take JSON and encode it as base64
+  const base64 = Buffer.from(sessionJSON).toString("base64");
+
+  // Part: Return a string that is the cookie with the encoded data
+  return [`session=${base64}`]; // Remark: Supertest expects an array of strings as a cookie.
+};
