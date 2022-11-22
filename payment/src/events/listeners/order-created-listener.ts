58b6031b7cb9ef0ece5cdaa422283511fa824bb5
Nayeem Nishaat <rxnayeemimi@gmail.com>
2022-11-22 15:36:25 +0600

    implemented order created listener

---
+++ payment/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,26 @@
+import {
+  Listener,
+  Subjects,
+  OrderCreatedEvent
+} from "@labyrinth-inc/ticketing-sdk";
+import { queueGroupName } from "./queue-group-name";
+import { Message } from "node-nats-streaming";
+import { Order } from "../../models/order";
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  readonly subject = Subjects.OrderCreated;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
+    const order = Order.build({
+      id: data.id,
+      version: data.version,
+      price: data.ticket.price,
+      userId: data.userId,
+      status: data.status
+    });
+    await order.save();
+
+    msg.ack();
+  }
+}
