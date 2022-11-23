b55e5a022fa2f19dc1135c4c71925d53535d8639
Nayeem Nishaat <rxnayeemimi@gmail.com>
2022-11-23 18:21:51 +0600

    created stripe charge

--- payment/src/routes/new.ts
+++ payment/src/routes/new.ts
@@ -9,6 +9,7 @@ import {
   OrderStatus
 } from "@labyrinth-inc/ticketing-sdk";
 import { Order } from "../models/order";
+import { stripe } from "../stripe";
 
 const router = express.Router();
 
@@ -28,6 +29,12 @@ router.post(
 
     if (order.status === OrderStatus.Cancelled)
       throw new BadRequestError("Cannot pay for a cancelled order");
+
+    await stripe.charges.create({
+      currency: "usd",
+      amount: order.price * 100,
+      source: token
+    });
 
     res.send({ success: true });
   }
