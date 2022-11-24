fcea66e1f63360d813318144ec4f867d509d714a
Nayeem Nishaat <rxnayeemimi@gmail.com>
2022-11-24 15:39:17 +0600

    fixed secret issue

--- payment/src/stripe.ts
+++ payment/src/stripe.ts
@@ -1,5 +1,6 @@
 import Stripe from "stripe";
 
-export const stripe = new Stripe(process.env.STRIPE_KEY!, {
+export const stripe = new Stripe(`${process.env.STRIPE_KEY}`, {
   apiVersion: "2022-11-15"
 });
+console.log(`${process.env.STRIPE_KEY}`);
