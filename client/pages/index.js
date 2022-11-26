f7464dc1825b55ae5ac4f3746b104f1b1d3652ea
Nayeem Nishaat <rxnayeemimi@gmail.com>
2022-11-26 23:42:31 +0600

    implemented showing of list of tickets

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,14 +1,30 @@
 // import buildClient from "../api/build-client";
 
-function LandingPage({ currentUser }) {
-  return currentUser ? (
-    <h1>You are signed in!</h1>
-  ) : (
-    <h1>You are not signed in!</h1>
+function LandingPage({ currentUser, tickets }) {
+  return (
+    <div>
+      <h1>Tickets</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tickets.map((ticket) => (
+            <tr key={ticket.id}>
+              <td>{ticket.title}</td>
+              <td>{ticket.price}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
-LandingPage.getInitialProps = async (_context, _client, _currentUser) => {
+LandingPage.getInitialProps = async (_context, client, _currentUser) => {
   // Important: `getInitialProps` Executed in the server when we visit/refresh the page.
   // Important: `getInitialProps` Executed in the client when we have already mounted the page on the client and navigate to the page somehow without revisiting/refreshing the page.
   // Note: To check if user is logged in or not the request is required to made in this serverside component.
@@ -19,7 +35,9 @@ LandingPage.getInitialProps = async (_context, _client, _currentUser) => {
   // Note: kubectl get services -n ingress-nginx -> Get all services of ingress-nginx namespace.
   // const { data } = await buildClient(context).get("/api/users/currentuser");
   // return data;
-  return {};
+  const { data } = await client.get("/api/tickets");
+
+  return { tickets: data };
 };
 
 export default LandingPage;
