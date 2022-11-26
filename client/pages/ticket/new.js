1ad5ecdfe3c107b276d0d689f13c3e3f0c6086d7
Nayeem Nishaat <rxnayeemimi@gmail.com>
2022-11-26 22:50:35 +0600

    form complete

--- client/pages/ticket/new.js
+++ client/pages/ticket/new.js
@@ -1,4 +1,9 @@
+import { useState } from "react";
+
 const NewTicket = () => {
+  const [title, setTitle] = useState("");
+  const [price, setPrice] = useState("");
+
   return (
     <div>
       <h1>Create a Ticket</h1>
@@ -6,11 +11,28 @@ const NewTicket = () => {
       <form>
         <div className="form-group">
           <label>Title</label>
-          <input className="form-control" type="text" />
+          <input
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className="form-control"
+            type="text"
+          />
         </div>
         <div className="form-group">
           <label>Price</label>
-          <input className="form-control" type="text" />
+          <input
+            type="number"
+            value={price}
+            onBlur={(e) => {
+              const value = parseFloat(e.target.value);
+
+              if (isNaN(value)) return;
+
+              setPrice(value.toFixed(2));
+            }}
+            onChange={(e) => setPrice(e.target.value)}
+            className="form-control"
+          />
         </div>
         <button className="btn btn-primary mt-3">Submit</button>
       </form>
